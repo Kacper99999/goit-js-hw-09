b163597f84de3054581ebd7a2042cba19df53f1c
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-04-17 21:25:09 +0200

    Update color-switcher.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const start = document.querySelector("[data-start]");
+const stop = document.querySelector("[data-stop");
+let warunek = null;
+function changeColor(){
+    document.body.style.backgroundColor = getRandomHexColor();
+};
 
+start.addEventListener("click", ()=>{
+    start.setAttribute("disabled","disabled");
+    warunek = true;
+    setInterval(() =>{
+        if(warunek){
+        changeColor();
+        }
+    },5000)
+
+});
+
+stop.addEventListener("click", (() =>{
+    start.removeAttribute("disabled");
+    warunek = false;
+}));
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
